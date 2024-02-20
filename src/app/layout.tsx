5374848c4468ded636baf457f568e5545c754de2
fabry-js <vivaldifabrizio10@gmail.com>
2024-02-20 14:26:04 +0100

    feat: change name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Auto Catalyst",
+  title: "HyperFlux",
   description: "",
 };
 
